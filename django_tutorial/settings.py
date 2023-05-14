0a38060ac211809568a01fca7e80034a417bbccd
whysyahrizal <wahyu.sahrijal@ui.ac.id>
2023-05-14 15:04:51 +0700

    tes

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+APP_NAME = 'money_tracker'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-sh535))ljcep)42pdypj!)7=&g^rfp8o)#f+icn%vg2hmttp-(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', f'{APP_NAME}.up.railway.app']
 CSRF_TRUSTED_ORIGINS = [f'https://money-tracker.up.railway.app']
 
 # Application definition
